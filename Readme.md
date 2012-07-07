f20113d3d5ec5b7f02fe3c8f4cca702f7903c56e
Florian Traverse <florian.traverse@gmail.com>
2012-07-07 11:38:06 +0200

    proper proj loading + readme is back -> 0.3.0

---
+++ Readme.md
@@ -0,0 +1,65 @@
+This is a library for node.js, build on the original Proj4JS sources made for thr the browser
+
+## Use Node.JS port of Proj4JS ##
+Without cloning this project you can use it directly using npm :
+```
+ npm install proj4js
+```
+In your JS :
+
+```javascript
+ var Proj4js = require("proj4js");
+```
+
+## Usage example##
+
+### Synchronous mode
+
+You should use either one of the provided projection which is stored in the "defs" subdirectory of the "lib" folder of the node Proj4JS project (have a look at your project node_modules folder), or an equivalent file in your own "defs" subdirectory relative to the directory containing the module requiring proj4js.
+
+```javascript
+var Proj4js = require("proj4js"),
+    proj = new Proj4js.Proj("EPSG:4139");
+     
+console.log(Proj4js.transform(Proj4js.WGS84, proj, new Proj4js.Point([45,5])));
+//returns
+//{ x: 44.999611051259926,
+//  y: 5.0013285897182325,
+//  z: -117.26076901517808 }
+
+```
+### Asychonous mode
+
+(Also works with local files, like synchronous mode)
+
+```javascript
+var Proj4js = require("proj4js"),
+    proj = new Proj4js.Proj("EPSG:2192", function() {
+        console.log(Proj4js.transform(Proj4js.WGS84, proj, new Proj4js.Point([45,5])));
+})
+     
+console.log(Proj4js.transform(Proj4js.WGS84, proj, new Proj4js.Point([45,5])));
+//returns
+//{ x: 6295336.596034983,
+//  y: -1241636.8411306776,
+//  z: -250.30824038013816 }
+
+```
+
+## Documentation ##
+
+For more documentation on Proj4JS usage, please read the original proj4js documentation : http://trac.osgeo.org/proj4js/
+
+## "build" node.js version ##
+```
+ git clone git://github.com/temsa/node-proj4js.git
+ cd node-proj4js
+ cd build
+ npm install async underscore mkdirp
+ node ./build.js
+ cd node # here is the resulting package !
+```
+then you can use it with npm link (see documentation) or do a
+```
+ npm install
+```
--- build/build.js
+++ build/build.js
@@ -23,9 +23,49 @@ module.exports = Proj4js;";
 function nodify(filename, content) {
 	content = content +"\n\
 var alert = console.error;\n\
+Proj4js.reportError = console.error;\n\
 Proj4js.getScriptLocation = function(){return './'};\n\
-Proj4js.loadScript = function(url, onload, onfail, check){onload(require(url));}\n\
-module.exports = Proj4js;\n" ;
+Proj4js.loadScript = function(url, onload, onfail, check){\n\
+  var URL = require('url').parse(url);\n\
+  if(URL.protocol) {\n\
+    require('http').get(URL, function(res){\n\
+      if(res.statusCode>=300) {\n\
+        if(Proj4js.debug) console.warn('this seems not to be valid projection, http statuscode :',res.statusCode)\n\
+        return onfail()\n\
+      }\n\
+      var body = '';\n\
+      res\n\
+        .on('data', function(chunk) {body+=chunk.toString()})\n\
+        .on('end', function() {\n\
+          var vm = require('vm');\n\
+          try {\n\
+            vm.runInContext(body, vm.createContext({Proj4js: Proj4js}, url.href));\n\
+          } catch (e) {\n\
+            if(Proj4js.debug) console.warn('this seems not to be valid JS :', body)\n\
+            onfail()\n\
+          }\n\
+          onload();\n\
+        })\n\
+        .on('error', function() {\n\
+          if(Proj4js.debug) console.warn('an http error happend while gettin ', URL.href)\n\
+          onfail()\n\
+        });\n\
+      });\n\
+  } else {\n\
+    try{\n\
+      onload(module.require(url)); //relative to this module\n\
+    } catch(e) {\n\
+      try {\n\
+        if(Proj4js.debug)  console.warn('did not found module', url, 'trying relative to the calling script')\n\
+        onload(module.parent.require(url)); //relative to the caller\n\
+      } catch(e) {\n\
+        if(Proj4js.debug)  console.warn('did not found the module locally, should now try on the internet; make sure to use the callback!')\n\
+        onfail()\n\
+      }\n\
+    }\n\
+  }\n\
+}\n\
+module.exports = Proj4js;" ;
 	write(filename, content);
 }
 
